ac2bac9ad764d68318234bdf5a33e27f77f34558
laspinto <71529948+laspinto@users.noreply.github.com>
2020-11-28 12:41:47 -0500

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -13,5 +13,24 @@
       $("#carouselButton").children("i").addClass("fa-pause"); 
       }
   });
-                                
+       
+  
+  
+    $("#loginButton").click(function(){
+      $("#loginModal").modal("show");
+  });
+
+  
+    $("#reserveButton").click (function(){
+      $ ("#reserveModal").modal("show");
+    });
+  
 });
+
+
+
+
+
+
+
+
